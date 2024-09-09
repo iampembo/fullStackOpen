46cb181974b5721bd37923de6cab5e1a1cf595dc
iampembo <103342913+iampembo@users.noreply.github.com>
2024-09-10 09:31:15 +1000

    Part 1a exercises complete

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
       <div>
         <Part part={part1} exercises={exercises1} />
         <Part part={part2} exercises={exercises2} />
-        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
       </div>
     )
   }
